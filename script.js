65644934bfd751e5f71eacf1c75feaffd181c7b3
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-12 23:52:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function makeid(l) {
   // write your code here
+	let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.l;
+
+  for (let i = 0; i < l; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+
+	
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
